29d7d1a51493890224d4836160511c61e4d110fd
ali <alikhaledm399@gmail.com>
2023-03-10 07:12:27 +0200

    pushing merge with same files but empty step1

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+  $servername = "localhost";
+  $dbusername = "root";
+  $dbpassword = "";
+  $dbname = "agencydb";
+
+  $conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+?>
--- signin.php
+++ signin.php
@@ -1,90 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <link rel="stylesheet" href="styles.css">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Homepage</title>
   <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="styles.css">
+  <!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $servername = "localhost";
-        $username = "root";
-        $dbpassword = "";
-        $dbname = "ubitdb";
-
-        $conn = mysqli_connect($servername, $username, $dbpassword, $dbname);
-
-        $name = $_POST["username"];
-        $password = $_POST["password"];
-
-        $sql = "SELECT * FROM user WHERE name='$name'";
-        $result = mysqli_query($conn, $sql);
-
-        if (mysqli_num_rows($result) > 0) {
-            $row = mysqli_fetch_assoc($result);
-            if ($row["password"] == $password) {
-                // Redirect to homepage
-                header("Location: AIO.html");
-                exit;
-            } else {
-                echo "Incorrect password";
-            }
-        } else {
-            echo "Username not found. Please create an account.";
-        }
-
-        $conn->close();
-    }
-?>
-
+  </head>
+  <body>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
+ 
 </head>
 <body>
   <!-- Navbar -->
 
-  <nav class="navbar navbar-expand-lg text-center">
-   
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li></li><li></li><li></li>
-        <li></li><li></li><li></li>
-        <li></li><li></li><li></li>
-        <li></li><li></li><li></li>
-        <li class="nav-item active">
-            <a class="nav-link navbar-brand" href="AIO.HTML"><img width=120 height=120 src="/images/UBIT.png" alt=""> <span class="sr-only"></span></a>
+ 
+
+  
+  
+  
+
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.html">
+        <img src="images/UBIT.png" width="100" height="100" alt="logo" class="d-inline-block align-top">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+       
+        <ul class="navbar-nav ms-auto">
+          <li></li><li></li><li></li><li></li><li></li><li></li><li><li><li></li></li></li>
+  
+
+          <li class="nav-item">
+            <a class="nav-link active text-center" aria-current="page" href="project.html"><button type="button" class="projectbtn btn-outline-secondary">Start A Project</button></a>
           </li>
-        <li class="nav-item">
-            <a class="nav-link navbar-brand" href="#">Home <span class="sr-only"></span></a>
+         
+        </ul>
+        <ul class="navbar-nav ms-auto text-center">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">About</a>
           </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About <span class="sr-only"></span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="signin.php">Sign in</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="signup.php">Sign up</a>
-        </li>
-      </ul>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="signinubit.html">Sign In</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="signupubit.html">Sign Up</a>
+          </li>
+          </ul>
+      </div>
     </div>
   </nav>
+
+  
 
   <!-- Body -->
 
 
-  <div class="social-icons">
+  <div class="social-icons hideformobile">
     
-<a href="facebook.com"> <img src="/images/facebook.svg"  alt=""> </a>
-<a href="facebook.com"> <img src="/images/instagram.svg"  alt=""> </a>
-  <a href="facebook.com"> <img src="/images/linkedin.svg"  alt=""> </a>
+<a href="facebook.com"> <img src="images/facebook.svg"  alt=""> </a>
+<a href="facebook.com"> <img src="images/instagram.svg"  alt=""> </a>
+  <a href="facebook.com"> <img src="images/linkedin.svg"  alt=""> </a>
 
   </div>
 
@@ -92,7 +84,7 @@
     <h1 class="text-center my-5">Sign in</h1>
     <div class="row justify-content-center">
       <div class="col-md-6">
-        <form action="signin.php" method="POST">
+        <form action="/signin" method="POST">
           <div class="form-group">
             <label for="username">Username:</label><br>
             <input type="text" id="username" name="username" class="form-control" required>
@@ -106,18 +98,20 @@
           </div>
           <br>
   
-          <button type="submit" class="btn btn-primary">Sign in</button>
+          <button type="submit" class="btncustom btn-primary">Sign in</button>
         </form>
-        <p class="text-center mt-3">Don't have an account? <a href="signup.php">Sign up</a></p>
+        <p class="text-center mt-3">Don't have an account? <a href="signupubit.html">Sign up</a></p>
       </div>
     </div>
   </div>
 
   
    <!-- Footer -->
-   <footer class="text-center">
-    <p>&copy; 2023 UBIT. All Rights Reserved.</p>
+   <footer class="text-center signfooter">
+    <p>
+      <a href="https://www.instagram.com/your_username"><img src="images/facebook.png" alt="Instagram" width="30" height="30"></a>
+      <a href="https://www.facebook.com/your_username"><img src="images/instagram.png" alt="Facebook" width="30" height="30"></a>
+      <a href="https://www.linkedin.com/in/your_username"><img src="images/linkedin.png" alt="LinkedIn" width="30" height="30"></a>
+    </p>
+    <p>&copy; 2023 UBIT. All Rights Reserved.</p> 
   </footer>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-</body>
-</html>
