10293096a2ba5cac25488f99eab9b90fae281770
yousef elfiky <yousefelfiky@gmail.com>
2023-03-11 11:40:58 +0200

    changes

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ include("navbar.php");
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
-<body>
+<body class="coverimage">
   <!----Link JAVASCRIPT-->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -29,7 +29,7 @@ include("navbar.php");
   </style>
   <div id="first-container" class="container centertext">
 
-    <div class="row coverimage">
+    <div class="row">
 
       <div class="col-md-6 mt-5">
         <h1 class="starting">Getting started can be<br>the hardest part</h1><br>
